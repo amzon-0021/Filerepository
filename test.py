4932de129f7394595d25d265fe5f32d2e74a6e53
amzon-0021 <58093149+amzon-0021@users.noreply.github.com>
2020-12-28 11:13:03 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,27 @@
-print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+print("""indasOS UpdateVer.9
+IndasOSをアップデートします
+""")
+input("続行するには何かキーを押してください")
+u = """
+import os
+import boot
+def sbl(t):
+    print('''
+    ===========================================
+                   INDASOS VER.9
+            |=========================|
+{t}Mode
+login>User
+password>user''')
+sbl("Nomal ")
+boot.boot()
+while True:
+    c = input("INDAS>>")
+    boot.cmd(c)
+    """
+
+s = u
+
+with open("SBL.py", mode='w') as f:
+    f.write(s)
+# New file
