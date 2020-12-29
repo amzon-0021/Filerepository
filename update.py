a1a98bb447ec16bfbbc7e47c87a621c7a3e23dfd
amzon-0021 <58093149+amzon-0021@users.noreply.github.com>
2020-12-29 18:22:24 +0900

    Create update.py

---
+++ update.py
@@ -0,0 +1,29 @@
+print("""
+カーネルに関する大型アップデート
+内容
+新しい次期INDASOS!
+
+INDASCOREをCoreVistに変更
+アプリケーションマネージャーStonappを実装
+UpdateマネージャーVistUpdateに変更
+互換性を保持するためのIACドライバーを実装
+cmdProをSCVに変更
+BootloderをOpenVistに変更
+合計kannelドライバー
+JapanesePach 2020_2
+IAC Ver.1
+ONDEMAD 3
+exeloder2
+CoreVist
+Stonapp
+OpenVist
+SCV
+RaMiesBareak
+Rams
+Linct
+Sart
+Dicritns
+PowerSystem
+""")
+input("これを実行するとSBLがアップデートプログラムで上書きされます 実行するにはキーを押してください")
+print("アップデートエラー")
